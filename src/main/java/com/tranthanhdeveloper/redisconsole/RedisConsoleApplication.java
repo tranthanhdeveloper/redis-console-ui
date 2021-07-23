75a3b132305a5175e3e7aa50634849613bd3e6c4
Thanh Tran <tranthanhdeveloper@gmail.com>
2021-07-23 14:00:53 +0700

    Source initial commit

---
+++ src/main/java/com/tranthanhdeveloper/redisconsole/RedisConsoleApplication.java
@@ -0,0 +1,13 @@
+package com.tranthanhdeveloper.redisconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisConsoleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisConsoleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tranthanhdeveloper/redisconsole/RedisConsoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tranthanhdeveloper.redisconsole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisConsoleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
